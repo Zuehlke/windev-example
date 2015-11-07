28dc9ce1b03e3392e0da9ab30f403f532c22b54c
Vassilis Rizopoulos <vassilisrizopoulos@gmail.com>
2015-11-07 20:07:14 +0100

    Script to download ChefDK

--- scripts/chef_check.cmd
+++ scripts/chef_check.cmd
@@ -11,6 +11,7 @@ echo Installing ChefDK
 set DEFAULT_CHEFDK_INSTALLER=software\chefdk-0.7.0-1.msi
 cd /D %~dp0/..
 @if "%CHEFDK_INSTALLER%"=="" set CHEFDK_INSTALLER=%DEFAULT_CHEFDK_INSTALLER%
+@if not exist %CHEFDK_INSTALLER% call download_chefdk.cmd
 %CHEFDK_INSTALLER% /qb
 @if errorlevel 1 goto error
 goto done
@@ -18,6 +19,7 @@ goto done
 echo ChefDK is not installed
 echo CHEFDK_INSTALLER was not defined 
 echo %DEFAULT_CHEFDK_INSTALLER% is missing
+echo and we failed to download ChefDK
 echo ERROR: cannot proceed without Chef
 goto done
 :done
